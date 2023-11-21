d69e9aae37b85d291b03dda14a19102eb05bb2b1
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-11-21 14:24:18 +0000

    Changes to be committed: 	modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Univerz',
   description: 'Shaping the future through education and innovation',
-  image:'https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="https://images.unsplash.com/photo-1523050854058-8df90110c9f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <meta property="og:image:width" />
+        <meta property="og:image:height"  />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
